564fbc669bdcc501bb6f2b51a3be075e6367875d
Taiyo Kohno <taiyowork66@gmail.com>
2023-12-07 00:47:34 +0900

    fix config

--- prettier.config.js
+++ prettier.config.js
@@ -1,23 +1,17 @@
 /** @type {import('prettier').Config} */
-// eslint-disable-next-line no-undef
-module.exports = {
-  importOrder: [
-    "^(react/(.*)$)|^(react$)",
-    "^(next/(.*)$)|^(next$)",
-    "",
-    "<THIRD_PARTY_MODULES>",
-    "",
-    "^types$",
-    "^@/components/(.*)$",
-    "^@/app/(.*)$",
-    "",
-    "^[./]",
-  ],
-  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
-  importOrderTypeScriptVersion: "5.0.4",
-
-  // @ianvs/prettier-plugin-sort-imports
-  plugins: ["@ianvs/prettier-plugin-sort-imports"],
-  // prettier
-  printWidth: 120,
-};
+export const importOrder = [
+  "^(react/(.*)$)|^(react$)",
+  "^(next/(.*)$)|^(next$)",
+  "",
+  "<THIRD_PARTY_MODULES>",
+  "",
+  "^types$",
+  "^@/components/(.*)$",
+  "^@/app/(.*)$",
+  "",
+  "^[./]",
+];
+export const importOrderParserPlugins = ["typescript", "jsx", "decorators-legacy"];
+export const importOrderTypeScriptVersion = "5.0.4";
+export const plugins = ["@ianvs/prettier-plugin-sort-imports"];
+export const printWidth = 120;
