8ba1fda019adc3c6cc76c2af2222143f986739c4
Taiyo Kohno <taiyowork66@gmail.com>
2023-12-07 01:06:17 +0900

    fix textbox testCode

--- src/components/TextBox.test.tsx
+++ src/components/TextBox.test.tsx
@@ -1,8 +1,18 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 
 import { TextBox } from "./TextBox";
 
-test("renders correctly", async () => {
-  const { container } = render(<TextBox value="value" onChange={() => {}} />);
-  expect(container).toMatchSnapshot();
+const onChangeMock = jest.fn();
+
+describe("TextBox", () => {
+  test("renders correctly dom", async () => {
+    render(<TextBox value="value" onChange={onChangeMock} />);
+    expect(screen.getByRole("textbox")).toBeTruthy();
+    expect(screen.getByRole("button", { name: /Search/i })).toBeTruthy();
+  });
+
+  test("renders correctly", async () => {
+    const { container } = render(<TextBox value="value" onChange={onChangeMock} />);
+    expect(container).toMatchSnapshot();
+  });
 });
