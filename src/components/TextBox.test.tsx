f329cd1a04e0b7622430282569a5f619430a6eac
Taiyo Kohno <taiyowork66@gmail.com>
2023-12-08 01:17:07 +0900

    update snapshots

--- src/components/TextBox.test.tsx
+++ src/components/TextBox.test.tsx
@@ -5,13 +5,13 @@ import { TextBox } from "./TextBox";
 const onChangeMock = jest.fn();
 
 describe("TextBox", () => {
-  test("renders correctly dom", async () => {
+  test("renders correctly dom", () => {
     render(<TextBox value="value" onChange={onChangeMock} />);
     expect(screen.getByRole("textbox")).toBeTruthy();
     expect(screen.getByRole("button", { name: /Search/i })).toBeTruthy();
   });
 
-  test("renders correctly", async () => {
+  test("renders correctly", () => {
     const { container } = render(<TextBox value="value" onChange={onChangeMock} />);
     expect(container).toMatchSnapshot();
   });
