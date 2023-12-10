3d1e5b2a0a8e2d2ed39cfe39441c7a93aa025f6c
Taiyo Kohno <taiyowork66@gmail.com>
2023-12-10 19:10:03 +0900

    fix separate homePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,17 @@
-import { useCallback, useEffect, useState } from "react";
-
 import "./App.scss";
 
 import { Route, Routes } from "react-router-dom";
 
-import { TextBox } from "./components/TextBox";
-import { getForecastWeather } from "./hooks/fetchers";
+import Date from "./page/Date/Date";
 import Home from "./page/Home/Home";
-import { ForecastFewDaysWeatherType } from "./types";
-
-const FORECAST_DAYS_AMOUNT = 5;
-const INITIAL_LOCATION = "New York";
 
 function App() {
-  const [text, setText] = useState(INITIAL_LOCATION);
-  const [errorMessage, setErrorMessage] = useState("");
-  const [forecastWeather, setForecastWeather] = useState<ForecastFewDaysWeatherType | undefined>();
-  const [isLoading, setIsLoading] = useState(false);
-
-  const fetchWeather = useCallback(async () => {
-    setIsLoading(true);
-    try {
-      const data = await getForecastWeather(text, FORECAST_DAYS_AMOUNT);
-      setErrorMessage("");
-      setForecastWeather(data);
-    } catch (err) {
-      if (err instanceof Error) {
-        setErrorMessage(err.message);
-      }
-      setForecastWeather(undefined);
-    } finally {
-      setIsLoading(false);
-    }
-  }, [text]);
-
-  useEffect(() => {
-    fetchWeather();
-  }, []);
-
-  const onSubmit = useCallback(
-    async (e: React.FormEvent<HTMLFormElement>) => {
-      e.preventDefault();
-      try {
-        if (!text) throw new Error("Please enter a location.");
-        setErrorMessage("");
-        await fetchWeather();
-      } catch (err) {
-        if (err instanceof Error) {
-          setErrorMessage(err.message);
-        }
-      }
-    },
-    [text],
-  );
-
   return (
     <main className="app">
-      <h1>Weather Forecast</h1>
-      <form onSubmit={onSubmit}>
-        <TextBox value={text} onChange={setText} />
-        {errorMessage && <p className="error">{errorMessage}</p>}
-      </form>
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : (
-        !errorMessage && (
-          <Routes>
-            <Route path="/" element={forecastWeather ? <Home forecastWeather={forecastWeather} /> : <p>No Data</p>} />
-          </Routes>
-        )
-      )}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:date" element={<Date />} />
+      </Routes>
     </main>
   );
 }
--- src/page/Home/Home.tsx
+++ src/page/Home/Home.tsx
@@ -1,42 +1,108 @@
+import "./Home.scss";
+
+import { useCallback, useEffect, useState } from "react";
+
+import { Link } from "react-router-dom";
+
+import { TextBox } from "../../components/TextBox";
+import { getForecastWeather } from "../../hooks/fetchers";
 import { ForecastFewDaysWeatherType } from "../../types";
 
 const formatDate = (date: string) => date.slice(5).replace("-", "/");
 
-function Home({ forecastWeather}: { forecastWeather: ForecastFewDaysWeatherType}) {
+const FORECAST_DAYS_AMOUNT = 5;
+const INITIAL_LOCATION = "New York";
+
+function Home() {
+  const [text, setText] = useState(INITIAL_LOCATION);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [forecastWeather, setForecastWeather] = useState<ForecastFewDaysWeatherType | undefined>();
+  const [isLoading, setIsLoading] = useState(false);
   const { current, forecast, location } = forecastWeather || {};
   const locationName = [location?.name, location?.region, location?.country].filter(Boolean).join(", ");
 
+  const fetchWeather = useCallback(async () => {
+    setIsLoading(true);
+    try {
+      const data = await getForecastWeather(text, FORECAST_DAYS_AMOUNT);
+      setErrorMessage("");
+      setForecastWeather(data);
+    } catch (err) {
+      if (err instanceof Error) {
+        setErrorMessage(err.message);
+      }
+      setForecastWeather(undefined);
+    } finally {
+      setIsLoading(false);
+    }
+  }, [text]);
+
+  const onSubmit = useCallback(
+    async (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      try {
+        if (!text) throw new Error("Please enter a location.");
+        setErrorMessage("");
+        await fetchWeather();
+      } catch (err) {
+        if (err instanceof Error) {
+          setErrorMessage(err.message);
+        }
+      }
+    },
+    [text],
+  );
+
+  useEffect(() => {
+    fetchWeather();
+  }, []);
+
   return (
     <>
-      {location && <h3>{`Weather in ${locationName} at ${location.localtime}`}</h3>}
-      {current && (
-        <div className="current">
-          <img
-            src={`https://${current.condition.icon}`}
-            className="current_icon"
-            alt={`${current.condition.text} Image`}
-          />
-          <p className="current__name">{current.condition.text}</p>
-          <p className="current__temp">
-            <span className="current__tempMain">{current.temp_c}</span>°C
-          </p>
-          <p>{`Humidity: ${current.humidity}%`}</p>
-          <p>{`Wind: ${current.wind_kph}kph`}</p>
-        </div>
-      )}
-      {forecast && (
-        <>
-          <h3>Next 5days weather</h3>
-          <ul className="forecast">
-            {forecast.forecastday.map((item) => (
-              <li key={item.date} className="forecast__item">
-                <p className="forecast__itemDate">{formatDate(item.date)}</p>
-                <img src={item.day.condition.icon} alt="" />
-                <p className="forecast__itemName">{item.day.condition.text}</p>
-              </li>
-            ))}
-          </ul>
-        </>
+      <h1>Weather Forecast</h1>
+      <form onSubmit={onSubmit}>
+        <TextBox value={text} onChange={setText} />
+        {errorMessage && <p className="error">{errorMessage}</p>}
+      </form>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        !errorMessage && (
+          <>
+            {location && <h3>{`Weather in ${locationName} at ${location.localtime}`}</h3>}
+            {current && (
+              <div className="current">
+                <img
+                  src={`https://${current.condition.icon}`}
+                  className="current_icon"
+                  alt={`${current.condition.text} Image`}
+                />
+                <p className="current__name">{current.condition.text}</p>
+                <p className="current__temp">
+                  <span className="current__tempMain">{current.temp_c}</span>°C
+                </p>
+                <p>{`Humidity: ${current.humidity}%`}</p>
+                <p>{`Wind: ${current.wind_kph}kph`}</p>
+              </div>
+            )}
+            {forecast && (
+              <>
+                <h3>Next 5days weather</h3>
+                <ul className="forecast">
+                  {forecast.forecastday.map((item) => (
+                    <Link to={`/${item.date}`} key={item.date}>
+                      <li className="forecast__item">
+                        <p className="forecast__itemDate">{formatDate(item.date)}</p>
+                        <img src={item.day.condition.icon} alt="" />
+                        <p className="forecast__itemName">{item.day.condition.text}</p>
+                      </li>
+                    </Link>
+                  ))}
+                </ul>
+              </>
+            )}
+          </>
+        )
       )}
     </>
   );
